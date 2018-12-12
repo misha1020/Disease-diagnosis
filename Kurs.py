e4e535463339050fb27c10640c90541c33242fcd
misha1020 <32141478+misha1020@users.noreply.github.com>
2018-12-12 19:04:57 +0500

    Turn, Blur, Binary, Square, Squeeze, ConvertPILtoOpenCV
    1. Бинаризация, устранение шумов и поворот выводятся на экран при помощи plt.
    2. Изображение квадрируется
    3. Вертеть можно изображение на указанное кол-во градусов
    4. Есть возможность изменять размер изображения

--- Kurs.py
+++ Kurs.py
@@ -1,89 +1,110 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt    
 from PIL import Image
+from matplotlib import pyplot as plt
 
-def make_square(old_path, new_path, max_size=600, fill_color=(0, 0, 0)):
-    # find image dimensions
-    old_img = Image.open(old_path)
-    size = (min(max_size, max(old_img.size)),) * 2
 
-    # resize if old image is larger than max_size
-    if size[0] < old_img.size[0] or size[1] < old_img.size[1]:
-        old_img.thumbnail(size)
+def Turn(img, val):
+    (h, w) = img.shape[:2]
+    center = (w / 2, h / 2)
+    
+    '''
+    M = cv2.getRotationMatrix2D(center, 90, 1.0)
+    tur90 = cv2.warpAffine(img, M, (w, h))
+    M = cv2.getRotationMatrix2D(center, 180, 1.0)
+    tur180 = cv2.warpAffine(img, M, (w, h))
+    M = cv2.getRotationMatrix2D(center, 270, 1.0)
+    tur270 = cv2.warpAffine(img, M, (w, h))
+    titles = ['Original Image', '90*', '180*', '270*']
+    images = [img, tur90, tur180, tur270]
+    for i in range(4):
+        plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+    '''
 
-    # create new image with the given color and computed size
-    new_img = Image.new(old_img.mode, size, fill_color)
+    M = cv2.getRotationMatrix2D(center, val, 1.0)
+    rotated = cv2.warpAffine(img, M, (w, h))
 
-    # find coordinates of upper-left corner to center the old image in the new image
-    assert new_img.size[0] >= old_img.size[0]
-    assert new_img.size[1] >= old_img.size[1]
+    #cv2.imwrite('testrotated.jpg',rotated)
+    return rotated
 
-    x = (new_img.size[0] - old_img.size[0]) // 2
-    y = (new_img.size[1] - old_img.size[1]) // 2
-
-    # paste image
-    new_img.paste(old_img, (x, y))
-    # save image
-    new_img.save(new_path)
 
 def Blur(img):
-    #blur = cv2.blur(img,(5,5)) 
-    blur = cv2.GaussianBlur(img,(9,9),0) 
-    #blur = cv2.medianBlur(img,5) 
-    #blur = cv2.bilateralFilter(img,9,75,75) 
+    blurGaus = cv2.GaussianBlur(img, (7, 7), 0) 
     
-    #plt.subplot(121),plt.imshow(img),plt.title('Original')         # paint on plt form
-    #plt.xticks([]), plt.yticks([]) 
-    #plt.subplot(122),plt.imshow(blur),plt.title('Blurred') 
-    #plt.xticks([]), plt.yticks([]) 
-    #plt.show()
-    return blur
-    #cv2.imwrite("Blured.png", blur)
-    #cv2.imshow("Blur image", blur)
-    #cv2.waitKey(0)
+    '''
+    kernel = np.ones((5,5),np.float32)/25
+    blur2DConv = cv2.filter2D(img,-1,kernel)
+    blurAver = cv2.blur(img, (5, 5)) 
+    blurMed = cv2.medianBlur(img, 5) 
+    blurBilFiltr = cv2.bilateralFilter(img, 9, 75, 75) 
+    titles = ['Original Image', 'Gaussian', '2D Convolution', 'Averaging', 'Median', 'BilateralFiltering']
+    images = [img, blurGaus, blur2DConv, blurAver, blurMed, blurBilFiltr]
+    for i in range(6):
+        plt.subplot(2,3,i+1),plt.imshow(images[i])
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+    '''
 
+    #cv2.imwrite('testblur.jpg',blurGaus)
+    return blurGaus
+
+
+def Binary(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thGlobal = cv2.threshold(imgGray, 165, 255, cv2.THRESH_BINARY)
+
+    '''
+    thAdaptMean = cv2.adaptiveThreshold(imgGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    thAdaptGaus = cv2.adaptiveThreshold(imgGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    titles = ['Original Image', 'Global Thresholding (v = 165)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+    images = [imgGray, thGlobal, thAdaptMean, thAdaptGaus]
+    for i in range(4):
+        plt.subplot(2,2,i+1),plt.imshow(images[i], 'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+    '''
+
+    #cv2.imwrite('testbin.jpg',thGlobal)
+    return thGlobal
+
+
+def Square(im, min_size=256, fill_color=(0, 0, 0, 0)):
+    x, y = im.size
+    size = max(min_size, x, y)
+    new_im = Image.new('RGBA', (size, size), fill_color)
+    new_im.paste(im, ((size - x) // 2, (size - y) // 2))
+    return new_im
+
+def ConverPILtoOpenCV(pil_image):
+    numpy_image = np.array(pil_image)  
+    opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR) 
+    return opencv_image
+
+
+def Squeeze(img, size):
+    wpercent = (size/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((size,hsize), Image.ANTIALIAS)
+    return img
 
 def main():
-    in_img = "dog.jpg"
-    image = cv2.imread(in_img)          # load image
-    Blur(image)
-    '''
-    (h, w) = image.shape[:2]
-    center = (w / 2, h / 2)
-    M = cv2.getRotationMatrix2D(center, 270, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h))
-    '''
-    #cv2.imwrite('lol.png',image)                # save image
-    cv2.imshow("Rotated image", image)          # show image
-    cv2.waitKey(0)                              # readkey
-    cv2.destroyAllWindows()                     # press enter to close all windows
-    
+    in_img = "test.jpg"
+    #image = cv2.imread(in_img)                   
+    image = Image.open(in_img)                  # load image
+    image = Square(image)
+    image = Squeeze(image, 1600)
+    image = ConverPILtoOpenCV(image)
+    image = Turn(image, 180)
+    image = Blur(image)
+    image = Binary(image)
+    cv2.imwrite('output.jpg',image)              # save image
+    #cv2.imshow("Rotated image", image)          # show image
+    #cv2.waitKey(0)                              # readkey
+    #cv2.destroyAllWindows()                     # press enter to close all windows
 
 main()
-
-'''
-mode = int(input('mode:'))
-image = Image.open("test.jpg")
-draw = ImageDraw.Draw(image)
-width = image.size[0]
-height = image.size[1]
-pix = image.load()
-
-if (mode == 5):
-	factor = int(input('factor:'))
-	for i in range(width):
-		for j in range(height):
-			a = pix[i, j][0]
-			b = pix[i, j][1]
-			c = pix[i, j][2]
-			S = a + b + c
-			if (S > (((255 + factor) // 2) * 3)):
-				a, b, c = 255, 255, 255
-			else:
-				a, b, c = 0, 0, 0
-			draw.point((i, j), (a, b, c))
-
-image.save("result200.jpg", "JPEG")
-del draw
-'''
